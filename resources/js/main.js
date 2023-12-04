af0a8260a94af3c15b137741e9e6bb0812e13bae
Omar Salem <omar.salem1415@gmail.com>
2023-12-04 22:14:16 +0000

    first try of dog api to load random images in the carousel

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,4 +1,46 @@
 $(document).ready(function(){
   $('.sidenav').sidenav();
   $('.collapsible').collapsible();
+  $('.carousel').carousel();
 });
+
+
+fetch("https://api.thedogapi.com/v1/images/search?limit=10")
+.then(resp => resp.json())
+.then(function(data) {
+  console.log(data);
+
+  let carousel = document.querySelector('.carousel');
+
+    // Loop through the array of image URLs and create carousel items
+    data.forEach((catImage, index) => {
+      // Create a new carousel item
+      const carouselItem = document.createElement('a');
+      carouselItem.classList.add('carousel-item');
+      carouselItem.href = '#' + (index + 1); // Set the href attribute
+    
+      // Create an image element and set its src attribute
+      const imgElement = document.createElement('img');
+      imgElement.src = catImage.url;
+    
+      // Append the image to the carousel item
+      carouselItem.appendChild(imgElement);
+    
+      // Append the carousel item to the carousel container
+      carousel.appendChild(carouselItem);
+    });
+
+    // Initialize Materialize Carousel
+    M.Carousel.init(carousel, {
+      fullWidth: true,
+      indicators: true,
+      dist: 0,
+      padding: 10
+    });
+
+    console.log(document.getElementsByTagName("img"));
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+
