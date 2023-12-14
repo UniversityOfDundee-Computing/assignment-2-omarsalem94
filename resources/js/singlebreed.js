b83a5afdc8ec1cc3a87553c46e9e7dfbcd0f785e
Omar Salem <omar.salem1415@gmail.com>
2023-12-14 01:54:53 +0000

    Added single breed structure and logic

---
+++ resources/js/singlebreed.js
@@ -0,0 +1,67 @@
+// Rules to activate materialize components
+$(document).ready(function(){
+  $('.sidenav').sidenav();
+  $('.collapsible').collapsible();
+  $('.carousel').carousel();
+});
+
+let api_key = 'api_key=live_5Z8ovOq9Ol9IKTqHssHvOlrvNYdzY1v6SPcoZ2xRXIOvLd8tdISWbowR9oh6y3wN';
+
+let searchString = window.location.href.split("?");
+searchString = searchString[1];
+
+let url = `https://api.thedogapi.com/v1/breeds/${searchString}&api_key=${api_key}`
+ 
+console.log(url);
+console.log(searchString);
+
+fetch(url)
+.then(resp => resp.json())
+.then(function (data) {
+  console.log(data);
+  // Create HTML structure
+  const dogDetailContainer = document.getElementById('dog-detail-container');
+
+  // Create header with the name of the dog
+  const header = document.createElement('h1');
+  header.textContent = data.name;
+  dogDetailContainer.appendChild(header);
+
+  // Create image element with the breed image
+  const image = document.createElement('img');
+  image.src = `https://cdn2.thedogapi.com/images/${data.reference_image_id}.jpg`;
+  image.style.maxHeight = '500px';
+  dogDetailContainer.appendChild(image);
+
+  // Create a table to display dog information using Materialize classes
+  const table = document.createElement('table');
+  // table.setAttribute('class', 'striped')
+  
+  const propertiesToDisplay = ['bred_for', 'breed_group', 'life_span', 'temperament'];
+
+  // Loop through the data and create list items
+  for (const property of propertiesToDisplay) {
+      const row = document.createElement('tr');
+      const propertyName = document.createElement('td');
+      const propertyValue = document.createElement('td');
+
+      propertyName.textContent = property;
+      propertyValue.textContent = data[property];
+
+      row.appendChild(propertyName);
+      row.appendChild(propertyValue);
+      table.appendChild(row);
+    }
+
+  // Append the list to the container
+  dogDetailContainer.appendChild(table);
+
+  const backButton = document.getElementById('back-button');
+  backButton.addEventListener('click', function () {
+  // Use the browser's history object to navigate back
+  window.history.back();
+  });
+})
+.catch(function(error) {
+  console.log(error);
+})
